3f83a281263f09165473212561b2cc9a1cb9b7cd
mikeonator <maudinyc@gmail.com>
2026-01-11 19:59:27 -0800

    Main Function block completed (menu (Tm))

--- Week1/extracreditdb/db_main.py
+++ Week1/extracreditdb/db_main.py
@@ -1,5 +1,5 @@
 #Michael Audi - Database Extra Credit Assignment Main Program Loop
-
+import db_functions as dbf
 
 course_db = {}
 
@@ -15,14 +15,64 @@ course_db["ANTH 254"] = {"name":"Magic, Witchcraft, Religion", "cred":4, "dept":
 course_db["AUT 101"] = {"name":"Basic Electricity-Automotive", "cred":2, "dept":"AUT", "desc":"A self paced course that provides understanding of fundamental principles of electricity"}
 course_db["AUT 281"] = {"name":"Hybrid Electric Vehicles II", "cred":4, "dept":"AUT", "desc":"A study of HEV (hybrid electric vehicles) and EV (electric vehicles) part 2"}
 
+def print_all():
+    return dbf.print_course_info(course_db)
+
+def print_individual():
+    coursenum = input("Input Course Number to print (Ex: CIS 161 or ANTH 254):")
+    return dbf.print_single_course(course_db, coursenum)
+
+def add_entry():
+    ##ask for each value
+    coursenum = input("Input Course Number to add (Ex: CIS 161 or ANTH 254) - ")
+    coursename = input("Input Course Name to add (Ex: Computer Science 1) - ")
+    coursecred = input("Input Course Credits to add (Ex: 4) - ")
+    try:
+        coursecred = int(coursecred)
+    except ValueError:
+        print("Type a number next time.")
+        return False    
+    coursedept = input("Input Course Department to add (Ex: CIS or ANT) (3 char max) - ")
+    coursedesc = input("Input Course Description to add (Ex: Algorithms, Algorithms, Algorithms!) - ")
+    return dbf.add_course(course_db, coursenum, coursename, coursecred, coursedept, coursedesc)
+
+def del_entry():
+    coursenum = input("Input Course Number to delete (Ex: CIS 161 or ANTH 254) - ")
+    return dbf.delete_course(course_db, coursenum)
+
+def print_dept():
+    coursedept = input("Input Course Department to print (Ex: CIS or ANT) (3 char CAPS) - ")
+    return dbf.print_department_courses(course_db, coursedept)
+
 def main():
+    print("Welcome to the Course Database Utility!")
     while True:
-        print("Welcome to the Course Database Utility! Please Choose an option: ")
+        print("\nPlease choose an option by typing the corresponding number:")
+        print("1: Print the existing database.")
+        print("2: Print individual entry based on Course Number")
+        print("3: Add a course to the database.")
+        print("4: Delete a course from the database.")
+        print("5: Print all courses based on a Department")
+        print("6: Exit Program")
 
-
-
-
-
+        choice = input("Option: ").strip()
+        if not choice.isdigit():
+            print("Incorrect input, please type a number from 1 to 6.")
+        else:
+            if choice == "1":
+                print_all()
+            elif choice == "2":
+                print_individual()
+            elif choice == "3":
+                add_entry()
+            elif choice == "4":
+                del_entry()
+            elif choice == "5":
+                print_dept()
+            elif choice == "6":
+                break
+            else:
+                print("Incorrect input, please type a number from 1 to 6.")
 
 if __name__ == "__main__":
     main()
