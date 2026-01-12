2282897393432654599876ea8c6aba95bac3505a
mikeonator <maudinyc@gmail.com>
2026-01-11 18:27:13 -0800

    Implemented add to database function

--- Week1/extracreditdb/extracreditdb.py
+++ Week1/extracreditdb/extracreditdb.py
@@ -50,3 +50,45 @@ def print_department_courses(inDict, dept):
     else:
         print_course_info(tempdb)
         return True
+
+def add_course(inDict, coursenum, coursename, coursecred, coursedept, coursedesc):
+        if  type(coursenum) != str:
+            print(f"Course Number must be a string <=8 characters. Type: {type(coursenum)}")
+            return False
+        elif len(coursenum) > 8:
+            print(f"Course Number must be a string <=8 characters. Len: {len(coursenum)}")
+            return False
+        elif coursenum in inDict:
+            print("Course Number already in database, please try again.")
+            return False
+        elif type(coursename) != str:
+            print(f"Course Name must be a string <=30 characters. Type: {type(coursename)}")
+            return False
+        elif len(coursename) > 30:
+            print(f"Course Name must be a string <=30 characters. Len: {len(coursename)}")
+            return False
+        elif type(coursecred) != int:
+            print(f"Course Credits does not match type int. Type: {type(coursecred)}")
+            return False
+        elif type(coursedept) != str:
+            print(f"Course Department must be a string 3 characters in length and all uppercase. Type: {type(coursedept)}")
+            return False
+        elif len(coursedept) != 3:
+            print(f"Course Department must be a string 3 characters in length and all uppercase. Len: {len(coursedept)}")
+            return False
+        elif coursedept.isupper() != True:
+            print(f"Course Department must be a string 3 characters in length and all uppercase. isupper: {coursedept.isupper()}")
+            return False
+        elif type(coursedesc) != str:
+            print(f"Course Description must be a string <=100 characters. Type: {type(coursedesc)}")
+            return False
+        elif len(coursedesc) > 100:
+            print(f"Course Description must be a string <=100 characters. Len: {len(coursedesc)}")
+            return False
+        elif coursenum[:3] != coursedept:
+            print(f"Course Number provided does not match the Course Department provided (when trimmed to first 3 characters)")
+            return False
+        else:
+            inDict[coursenum] = {"name":coursename, "cred":coursecred, "dept":coursedept, "desc":coursedesc}
+            return True
+
