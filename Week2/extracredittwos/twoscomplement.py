196eeebca35f61b2b65628aa78adda337fbb0527
mikeonator <maudinyc@gmail.com>
2026-01-14 13:32:37 -0800

    Extra Credit - Welp, i did this wrong because I got overzealous with google. time to do decimal to twos not binary to twos

--- Week2/extracredittwos/twoscomplement.py
+++ Week2/extracredittwos/twoscomplement.py
@@ -1,9 +1,21 @@
-#Two's Complement Functions 
+#Michael Audi - Two's Complement Function
 
 def twoscomplement(num: str):
+    '''
+    Docstring for twoscomplement
+    
+    :param num: Input a string containing a binary number of any length (EX: '010111010')
+    :type num: str
+    '''
     bits = int(num, 2)
     mask = (2 ** len(num)) - 1
     inverted = bits ^ mask
     complement = inverted +1
     return format(complement, f"0{len(num)}b")
 
+def main():
+    print(twoscomplement(input("Input a binary number: ")))
+
+
+if __name__ == "__main__":
+    main()
--- Week2/week2main.py
+++ Week2/week2main.py
@@ -1,122 +1,128 @@
 #Michael Audi - CIS161 Week 2 Main Assignment
 
-##Part 1
 
-print("--Part 1--")
 
-###Step 1
+def main():
+    ##Part 1
 
-x = 31
+    print("--Part 1--")
 
-print("Step 1")
-print(f"x = {x} | bin(x) = {bin(x)} | hex(x) = {hex(x)}")
+    ###Step 1
 
-###Step 2
+    x = 31
 
-x = 1.825
-
-print(f"{'':-^10}")
-print("Step 2")
-try:
+    print("Step 1")
     print(f"x = {x} | bin(x) = {bin(x)} | hex(x) = {hex(x)}")
-except TypeError:
-    print("if x = 1.825")
-    print("TypeError: 'float' object cannot be interpreted as an integer")
-    print("bin() and hex() can only accept integers, not floats.")
 
-x = 18
+    ###Step 2
 
-print(f"x = {x} | bin(x) = {bin(x)} | hex(x) = {hex(x)}")
+    x = 1.825
 
-###Step 3
+    print(f"{'':-^10}")
+    print("Step 2")
+    try:
+        print(f"x = {x} | bin(x) = {bin(x)} | hex(x) = {hex(x)}")
+    except TypeError:
+        print("if x = 1.825")
+        print("TypeError: 'float' object cannot be interpreted as an integer")
+        print("bin() and hex() can only accept integers, not floats.")
 
-y = 0b1011
+    x = 18
 
-z = 0xA3
+    print(f"x = {x} | bin(x) = {bin(x)} | hex(x) = {hex(x)}")
 
-print(f"{'':-^10}")
-print("Step 3")
-print(f"int(y) = {int(y)} | bin(y) = {bin(y)} | hex(y) = {hex(y)}")
-print(f"int(z) = {int(z)} | bin(z) = {bin(z)} | hex(z) = {hex(z)}")
+    ###Step 3
 
-###Step 4
+    y = 0b1011
 
-w = x + y + z
+    z = 0xA3
 
-print(f"{'':-^10}")
-print("Step 4")
-print(f"The Sum of x ({x}), y ({y}), and z ({z}) is: {w}")
+    print(f"{'':-^10}")
+    print("Step 3")
+    print(f"int(y) = {int(y)} | bin(y) = {bin(y)} | hex(y) = {hex(y)}")
+    print(f"int(z) = {int(z)} | bin(z) = {bin(z)} | hex(z) = {hex(z)}")
 
-##Compression
+    ###Step 4
 
-print(f"{'':-^10}")
-print("--Compression--")
+    w = x + y + z
 
-og_size = 240
-dict_size = 25
-comp_text_size = 148
+    print(f"{'':-^10}")
+    print("Step 4")
+    print(f"The Sum of x ({x}), y ({y}), and z ({z}) is: {w}")
 
-#Step 1 - Compression percentage formula
-def compression(og:int,dsize:int,compressed:int):
-    return (1 - ((compressed + dsize)/og))
+    ##Compression
 
-print(f"{'':-^10}")
-print("Step 1 and 2")
+    print(f"{'':-^10}")
+    print("--Compression--")
 
-#Step 2 - Calculate the compression percentage for the given variables
-compress = compression(og_size, dict_size, comp_text_size)
-print(compress)
+    og_size = 240
+    dict_size = 25
+    comp_text_size = 148
 
-print(f"{'':-^10}")
-print("Step 3")
+    #Step 1 - Compression percentage formula
+    def compression(og:int,dsize:int,compressed:int):
+        return (1 - ((compressed + dsize)/og))
 
-print(f"{'Compressed Text Size:':>21} {comp_text_size:<} characters")
-print(f"{'Dictionary Size:':>21} {dict_size:<} characters")
-print(f"{'Total Characters:':>21} {(comp_text_size+dict_size):<} characters")
-print(f"{'Original Text Size:':>21} {og_size:<} characters")
-print(f"{'Compression:':>21} {(compress*100):<.2f}%")
+    print(f"{'':-^10}")
+    print("Step 1 and 2")
 
-print(f"{'':-^10}")
-print("Step 3 Again!")
+    #Step 2 - Calculate the compression percentage for the given variables
+    compress = compression(og_size, dict_size, comp_text_size)
+    print(compress)
 
-og_size = 3201
-dict_size = 402
-comp_text_size = 1999
+    print(f"{'':-^10}")
+    print("Step 3")
 
-compress = compression(og_size, dict_size, comp_text_size)
+    print(f"{'Compressed Text Size:':>21} {comp_text_size:<} characters")
+    print(f"{'Dictionary Size:':>21} {dict_size:<} characters")
+    print(f"{'Total Characters:':>21} {(comp_text_size+dict_size):<} characters")
+    print(f"{'Original Text Size:':>21} {og_size:<} characters")
+    print(f"{'Compression:':>21} {(compress*100):<.2f}%")
 
-print(f"{'Compressed Text Size:':>21} {comp_text_size:<} characters")
-print(f"{'Dictionary Size:':>21} {dict_size:<} characters")
-print(f"{'Total Characters:':>21} {(comp_text_size+dict_size):<} characters")
-print(f"{'Original Text Size:':>21} {og_size:<} characters")
-print(f"{'Compression:':>21} {(compress*100):<.2f}%")
+    print(f"{'':-^10}")
+    print("Step 3 Again!")
 
-print(f"{'':-^10}")
-print("Step 3 Again!!")
+    og_size = 3201
+    dict_size = 402
+    comp_text_size = 1999
 
-og_size = 290120
-dict_size = 23205
-comp_text_size = 210384
+    compress = compression(og_size, dict_size, comp_text_size)
 
-compress = compression(og_size, dict_size, comp_text_size)
+    print(f"{'Compressed Text Size:':>21} {comp_text_size:<} characters")
+    print(f"{'Dictionary Size:':>21} {dict_size:<} characters")
+    print(f"{'Total Characters:':>21} {(comp_text_size+dict_size):<} characters")
+    print(f"{'Original Text Size:':>21} {og_size:<} characters")
+    print(f"{'Compression:':>21} {(compress*100):<.2f}%")
 
-print(f"{'Compressed Text Size:':>21} {comp_text_size:<} characters")
-print(f"{'Dictionary Size:':>21} {dict_size:<} characters")
-print(f"{'Total Characters:':>21} {(comp_text_size+dict_size):<} characters")
-print(f"{'Original Text Size:':>21} {og_size:<} characters")
-print(f"{'Compression:':>21} {(compress*100):<.2f}%")
+    print(f"{'':-^10}")
+    print("Step 3 Again!!")
 
-print(f"{'':-^10}")
-print("Step 3 Again!!!")
+    og_size = 290120
+    dict_size = 23205
+    comp_text_size = 210384
 
-og_size = 25
-dict_size = 5
-comp_text_size = 15
+    compress = compression(og_size, dict_size, comp_text_size)
 
-compress = compression(og_size, dict_size, comp_text_size)
+    print(f"{'Compressed Text Size:':>21} {comp_text_size:<} characters")
+    print(f"{'Dictionary Size:':>21} {dict_size:<} characters")
+    print(f"{'Total Characters:':>21} {(comp_text_size+dict_size):<} characters")
+    print(f"{'Original Text Size:':>21} {og_size:<} characters")
+    print(f"{'Compression:':>21} {(compress*100):<.2f}%")
 
-print(f"{'Compressed Text Size:':>21} {comp_text_size:<} characters")
-print(f"{'Dictionary Size:':>21} {dict_size:<} characters")
-print(f"{'Total Characters:':>21} {(comp_text_size+dict_size):<} characters")
-print(f"{'Original Text Size:':>21} {og_size:<} characters")
-print(f"{'Compression:':>21} {(compress*100):<.2f}%")
+    print(f"{'':-^10}")
+    print("Step 3 Again!!!")
+
+    og_size = 25
+    dict_size = 5
+    comp_text_size = 15
+
+    compress = compression(og_size, dict_size, comp_text_size)
+
+    print(f"{'Compressed Text Size:':>21} {comp_text_size:<} characters")
+    print(f"{'Dictionary Size:':>21} {dict_size:<} characters")
+    print(f"{'Total Characters:':>21} {(comp_text_size+dict_size):<} characters")
+    print(f"{'Original Text Size:':>21} {og_size:<} characters")
+    print(f"{'Compression:':>21} {(compress*100):<.2f}%")
+
+if __name__ == "__main__":
+    main()
