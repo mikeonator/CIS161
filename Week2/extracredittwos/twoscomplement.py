3f4329f172c138070493d22c8b4552709eb171c6
mikeonator <maudinyc@gmail.com>
2026-01-14 13:27:27 -0800

    Extra Credit - YOOO this was surprisingly functional lets gooOOOO

--- Week2/extracredittwos/twoscomplement.py
+++ Week2/extracredittwos/twoscomplement.py
@@ -1,8 +1,9 @@
 #Two's Complement Functions 
 
-def invertbits(num: str):
+def twoscomplement(num: str):
     bits = int(num, 2)
     mask = (2 ** len(num)) - 1
     inverted = bits ^ mask
-    return format(inverted, f"0{len(num)}b")
+    complement = inverted +1
+    return format(complement, f"0{len(num)}b")
 
