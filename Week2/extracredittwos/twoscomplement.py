2ef08c6fcca52fb0bf74c1580637886fe4e2acf4
mikeonator <maudinyc@gmail.com>
2026-01-15 10:40:03 -0800

    Extra Credit - Completed Pretty sure

--- Week2/extracredittwos/twoscomplement.py
+++ Week2/extracredittwos/twoscomplement.py
@@ -1,20 +1,30 @@
 #Michael Audi - Two's Complement Function
 
-def twoscomplement(num: str):
+def twoscomplement(InNum: str):
     '''
-    Docstring for twoscomplement
+    Input a string containing a decimal number between -128 and 127 and recieve a string containing the Two's complement of that decimal.
     
-    :param num: Input a string containing a binary number of any length (EX: '010111010')
+    :param num: Input a string containing a decimal number between -128 and 127.
     :type num: str
+    :return str: String containing the Two's Complement of the input decimal number.
     '''
-    bits = int(num, 2)
-    mask = (2 ** len(num)) - 1
-    inverted = bits ^ mask
-    complement = inverted +1
-    return format(complement, f"0{len(num)}b")
+
+    num = int(InNum)
+    if (num > 127) or (num < -128):
+        return "ERROR | Please input a decimal number (max 127 | min -128)."
+    elif (0 <= num <= 127):
+        #if number is positive, return formatted string containing the binary representation of the positive number.
+        return (f"{bin(num)[2:]:0>8}")
+    elif (-128 <= num < 0):
+        #if number is negative, perform twos complement action on the binary representation of the negative number as a positive (ykwim)
+        bits = abs(num)
+        mask = ((2 ** 8) - 1)
+        inverted = bits ^ mask
+        complement = inverted + 1
+        return (f"{bin(complement)[2:]:0>8}")
 
 def main():
-    print(twoscomplement(input("Input a binary number: ")))
+    print(twoscomplement(input("Please input a decimal number (max 127 | min -128): ")))
 
 
 if __name__ == "__main__":
