50a1b1e85184c15310df00a9ea9fb92ed5d88fa2
mikeonator <maudinyc@gmail.com>
2026-01-19 19:54:01 -0800

    Main - Step 1 and 2 completion

--- Week3/week3main.py
+++ Week3/week3main.py
@@ -1,1 +1,26 @@
 #Michael Audi - CIS161 Week 3 Main Assignment
+
+def main():
+
+    ##Step 1
+    print("--- Step 1 ---")
+    name = input("What's your name?\n")
+    print(f"Hello {name}!")
+
+    ##Step 2
+    print("\n--- Step 2 ---")
+    age = input("What's your age?\n")
+    
+    try:
+        print(f"Your age in 5 years would be {age + 5}")
+    except TypeError:
+        print('     TypeError: can only concatenate str (not "int") to str')
+        print('     Because age is an str, the + is being treated as string concatenation, not as addition.\n')
+        print(f"Your age in 5 years would be {(int(age) + 5)}")
+    
+    ##Step 3
+    print("\n--- Step 3 ---")
+    
+
+if __name__ == "__main__":
+    main()
