d3fa6de43749db4f7612105e6bb08993c8c52bb9
mikeonator <maudinyc@gmail.com>
2026-01-19 21:14:28 -0800

    Extra Credit - Step 3 - Death Date lmfao

--- Week3/week3main.py
+++ Week3/week3main.py
@@ -1,5 +1,7 @@
 #Michael Audi - CIS161 Week 3 Main Assignment
 
+import lifeexpectancy as le
+
 def main():
 
     ##Step 1
@@ -14,15 +16,20 @@ def main():
     try:
         print(f"Your age in 5 years would be {age + 5}")
     except TypeError:
-        print('     TypeError: can only concatenate str (not "int") to str')
-        print('     Because age is an str, the + is being treated as string concatenation, not as addition.\n')
+        print('TypeError: can only concatenate str (not "int") to str')
+        print('Because age is an str, the + is being treated as string concatenation, not as addition.\n')
         print(f"Your age in 5 years would be {(int(age) + 5)}")
     
     ##Step 3
     print("\n--- Step 3 ---")
     addto = input("How many years do you wish to add to your age?\n")
     print(f"Your age in {addto} years would be {int(addto) + int(age)}")
-    
+
+    ##Step 3 Extra Credit
+    print(f"Based on the Actuarial Life Table as used in the 2025 Trustees Report by the Social Security Administration")
+    print(f"You are expected to die in the year {le.life_expectancy}!")
+
+
 
 if __name__ == "__main__":
     main()
