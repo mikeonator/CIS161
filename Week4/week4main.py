0a3c6f2cd0bf495c5dc54171d04fc67337a217d3
mikeonator <maudinyc@gmail.com>
2026-01-30 14:20:06 -0800

    Main - Step 4

--- Week4/week4main.py
+++ Week4/week4main.py
@@ -20,3 +20,15 @@ try:
     print(num1)
 except NameError:
     print("NameError: name 'num1' is not defined")
+
+
+## Step 4
+
+def dog_age(dog_years):
+    return (24 + (dog_years - 2) * 4)
+
+input1 = 5
+input2 = 11
+print(f"{input1} dog years is equivalent to {dog_age(input1)} human years.")
+print(f"{input2} dog years is equivalent to {dog_age(input2)} human years.")
+
