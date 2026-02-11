54997bb89aa2bff09f7dd70510342440a609b521
mikeonator <maudinyc@gmail.com>
2026-02-11 05:52:48 +0000

    Main - Step 4.2, 5, 6

--- Week6/week6main.py
+++ Week6/week6main.py
@@ -24,14 +24,44 @@ def step3(x:int,y:int):
         x = x - y
     print("Blastoff!!")
 
-def step4():
+def step4_1():
     while True:
         wordin = input("Input a word: ")
         if len(wordin) < 5:
+            print("Goodbye!")
             break
         print(f"{wordin} has {len(wordin)} letters.")
-    print("Goodbye!")
 
+
+def step4_2():
+    count = 0
+    while True:
+        wordin = input("Input a word: ")
+        if len(wordin) < 5:
+            print("Goodbye!")
+            break
+        elif count == 4:
+            print("Loser!")
+            break
+        print(f"{wordin} has {len(wordin)} letters.")
+        count += 1
+
+def step5():
+    count = 10
+    while count < 101:
+        print(f"{count:>5} | {bin(count):<9} | {hex(count):<5}")
+        count += 1
+
+def step6iterate(x:int):
+    if x == 0:
+        return
+    print(x*"*")
+    step6iterate(x-1)
+
+def step6recursion(x:int):
+    while x != 0:
+        print(x*"*")
+        x -= 1
 
 
 def main():
@@ -48,9 +78,20 @@ def main():
     step3(int(input("Enter an integer: ")),int(input("Enter a decrease amount: ")))
 
     # Step 4
-    print("\n----- Step 4 -----")
-    step4()
+    print("\n----- Step 4.1 -----")
+    step4_1()
 
+    print("\n----- Step 4.2 -----")
+    step4_2()
+
+    print("\n----- Step 5 -----")
+    step5()
+
+    print("\n----- Step 6 -----")
+    print("Iteration:")
+    step6iterate(5)
+    print("Recursion:")
+    step6recursion(5)
 
 if __name__ == "__main__":
     main()
