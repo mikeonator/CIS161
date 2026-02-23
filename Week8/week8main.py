066b84b70c164b28d1fba7949689cb7ca944e452
mikeonator <maudinyc@gmail.com>
2026-02-23 18:49:57 +0000

    Main - Step 5 and Docstringing

--- Week8/week8main.py
+++ Week8/week8main.py
@@ -1,15 +1,25 @@
 # Michael Audi - CIS161 Week 8 Main Assignment
 
+from math import factorial as vroom
+import time
 
 def stop_shouting():
+    '''
+    if the second userinput is the all uppercase version of the first,
+    chastise them. if it isnt, chastise them. No mercy.
+    '''
     firstin = input("Enter a phrase: ")
-    secondin = input("Enter a phrase: ")
+    secondin = input("Enter the same phrase but louder: ")
     if firstin.upper() == secondin:
         return ("Stop shouting please!")
     else:
         return ("You didn't do as you were asked... sigh.")
 
 def vowel_check(x:str):
+    '''
+    calculates the total number of vowels
+    in the given string
+    '''
     vowels = ['a','e','i','o','u']
     count = 0
     for char in x:
@@ -20,6 +30,11 @@ def vowel_check(x:str):
     return count
 
 def vowel_ask():
+    '''
+    ask the user for a string to count the vowels of.
+    uses vowel_check() to do so.
+    prints the results.
+    '''
     string = input("Enter a string: ")
     count = vowel_check(string)
     if count == 0:
@@ -30,12 +45,20 @@ def vowel_ask():
         return (f"There are {count} vowels in the string {string}.")
 
 def str_compare(x:str,y:str):
+    '''
+    alphabetical string comparison.
+    like an "a comes before b" comparison.
+    '''
     if x < y:
         return True
     else:
         return False
 
 def str_ask():
+    '''
+    asking the user for two strings.
+    compares them using str_compare().
+    '''
     ask = input("Enter a string: ")
     ask2 = input("Enter another string: ")
     if str_compare(ask, ask2):
@@ -44,6 +67,10 @@ def str_ask():
         return (f"{ask} does not come before {ask2}.")
 
 def email_ask():
+    '''
+    email comparison function.
+    type it the same twice lol.
+    '''
     while(True):
         ask = input("------Enter your email address: ")
         ask2 = input("Enter your email address again: ")
@@ -55,8 +82,73 @@ def email_ask():
             break
     return
 
+def iter_vroom(x:int):
+    '''
+    iterative factorial on x.
+    '''
+    y = 1
+    for i in range(1, x + 1):
+        y *= i
+    return y
+
+def recurse_vroom(x:int):
+    '''
+    recursive factorial on x.
+    '''
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return x * recurse_vroom(x-1)
+    
+def math_vroom(x:int):
+    '''
+    math.factorial on x.
+    '''
+    return vroom(x)
+
+def vroom_race(x:int,func):
+    '''
+    times the execution of the provided function while
+    passing said function the int x as its argument.
+    returns [output of function, int time of exec in ns]
+    '''
+    start = time.perf_counter_ns()
+    numout = func(x)
+    stop = time.perf_counter_ns()
+    return (numout), (stop - start)
+
+def vroom_races(userin:int):
+    '''
+    will test and print the timed results of the iterative,
+    recursive, and python module factorial functions
+    using 3 predetermined and one user-provided value.
+    '''
+    vrooms = [iter_vroom, recurse_vroom, math_vroom]
+    vroom_names = ["Iterative","Recursive","python.math"]
+    inputs = [3,10,25,userin]
+    
+    #rangelimiter
+    if userin > 100:
+        raise ValueError
+
+    #warmup
+    for i in range(0,3):
+        vrooms[i](1)
+
+    print(f"{'Type':>12} {'':-^3} {'Input':^6} {'':-^3} {'Time (ns)':<12} {'':-^3} {'Output':^6}")
+    #iterate thru inputs and functions
+    for i in range(0,4):
+        print(f"{'':-^50}")
+        for p in range(0,3):
+            result = vroom_race(inputs[i],vrooms[p])
+            print(f"{vroom_names[p]:>12} {'':-^3} {inputs[i]:>6} {'':-^3} {result[1]:<12} {'':-^3} {result[0]:<6g}")
+    return
 
 def main():
+    '''
+    calls each step of the python assignment
+    '''
+    
     print("\n----- Step 1 -----")
     print(stop_shouting())
 
@@ -68,6 +160,9 @@ def main():
 
     print("\n----- Step 4 -----")
     email_ask()
+
+    print("\n----- Step 5 ------")
+    vroom_races(int(input("Enter an integer (<100) for factorial function racing: ")))
 
 if __name__ == "__main__":
     main()
