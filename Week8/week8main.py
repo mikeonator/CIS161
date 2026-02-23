8204158b31badba858ccaef3a6a595dbbc784d95
mikeonator <maudinyc@gmail.com>
2026-02-23 05:56:59 +0000

    Main - Step 1

--- Week8/week8main.py
+++ Week8/week8main.py
@@ -1,1 +1,17 @@
 # Michael Audi - CIS161 Week 8 Main Assignment
+
+
+def stop_shouting():
+    firstin = input("Enter a phrase: ")
+    secondin = input("Enter a phrase: ")
+    if firstin.upper() == secondin:
+        print("Stop shouting please!")
+    return
+
+def main():
+    print("\n----- Step 1 -----")
+    stop_shouting()
+    return
+
+if __name__ == "__main__":
+    main()
