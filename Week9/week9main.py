a18a652a9c26ad56e0e3f1abe82d84011bf45efc
mikeonator <maudinyc@gmail.com>
2026-03-04 14:21:11 -0800

    Main - Step 1 & Step 2

--- Week9/week9main.py
+++ Week9/week9main.py
@@ -1,1 +1,47 @@
 # Michael Audi - CIS161 Week 9 Main Assignment
+
+class Students:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+    
+    def hello(self):
+        print(f"Hello, my name is {self.name}. I'm {self.age} years old, and I'm in grade {self.grade}!")
+
+class Staff:
+    def __init__(self, name, role, department, salary):
+        self.name = name
+        self.role = role
+        self.department = department
+        self.salary = salary
+
+class Teacher(Staff):
+    def __init__(self, name, age, role, department, salary):
+        super().__init__(name, role, department, salary)
+        self.age = age
+    
+    def hello(self):
+        print(f"Hello, my name is {self.name}. I'm {self.age} years old, and I'm a {self.role} in the {self.department} department. My salary is {self.salary}!")
+
+def main():
+    # Step 1
+    print(f"----- Step 1 -----")
+    stu_raj = Students("Raj",16,11)
+    stu_joe = Students("Joe",17,11)
+
+    stu_raj.hello()
+    stu_joe.hello()
+
+    # Step 2
+    print(f"\n----- Step 2 -----")
+    tea_james = Teacher("James",32,"Teacher","Computer Science",80000)
+    tea_chris = Teacher("Chris",41,"Teacher","Music",80000)
+
+    tea_james.hello()
+    tea_chris.hello()
+
+    return
+
+if __name__ == "__main__":
+    main()
