44061c1b01312b48c764854e8c63524cbde7bbe6
mikeonator <maudinyc@gmail.com>
2026-03-10 14:02:27 -0700

    Main - Create SolarObject Class

--- Week10/week10main.py
+++ Week10/week10main.py
@@ -1,1 +1,13 @@
 # Michael Audi - CIS161 Week 10 Main Assignment
+
+class SolarObject:
+    def __init__(self, FarthestSunDistance, Spin, OrbitDays):
+        '''
+        Intialize class SolarObject containing:
+        Farthest distance from the sun in au
+        spin
+        and the time to orbit whatever it is orbiting(average) in days.
+        '''
+        self.FarthestSunDistance = FarthestSunDistance
+        self.Spin = Spin
+        self.OrbitDays = OrbitDays
